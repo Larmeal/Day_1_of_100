02ee7f87eeb266b5caebf58226426657a6085f66
Larmeal <armloveyou252@hotmail.com>
2021-12-12 10:49:07 +0700

    Project_Excercise_of_The_100_Day_Python_Coruse

---
+++ Project_Excercise.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+Name_City = input("What's name of the city you grew up in?\n")
+Name_Pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + Name_City + " " + Name_Pet)
